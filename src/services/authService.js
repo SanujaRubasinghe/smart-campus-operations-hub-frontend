44ef8eff46e52c0db7a1c06aa2d705679cf9662b
Javid Mushtan <javidmushtan@gmail.com>
2026-04-20 17:53:19 +0530

    add authentication service implementation

---
+++ src/services/authService.js
@@ -0,0 +1,21 @@
+import api from "./api";
+
+export const getCurrentUser = async () => {
+  const response = await api.get("/api/auth/me");
+  return response.data;
+};
+
+// To trigger login, normally you'd just redirect:
+export const triggerGoogleLogin = () => {
+  window.location.href = "http://localhost:8080/oauth2/authorization/google";
+};
+
+export const triggerGithubLogin = () => {
+  window.location.href = "http://localhost:8080/oauth2/authorization/github";
+};
+
+// If email/password was implemented:
+export const login = async (credentials) => {
+  const response = await api.post("/api/auth/login", credentials);
+  return response.data;
+};
